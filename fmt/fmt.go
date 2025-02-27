3c2550889fa77ed32b7335e118ebcacfd08dd1fe
k1LoW <k1lowxb@gmail.com>
2025-02-27 11:36:16 +0900

    feat: Support format with interpolation

--- fmt/fmt.go
+++ fmt/fmt.go
@@ -52,35 +52,72 @@ func (c *Cue) Format(in []byte) ([]byte, error) {
 		if field, ok := n.(*ast.Field); ok {
 			if ident, ok := field.Label.(*ast.Ident); ok {
 				if fmtcmd, ok := c.fmtcmds[ident.Name]; ok {
-					if strLit, ok := field.Value.(*ast.BasicLit); ok && strLit.Kind == token.STRING {
-						// multiline string literal only
-						if !strings.Contains(strLit.Value, "\n") || !strings.Contains(strLit.Value, `"""`) {
-							return
-						}
-						v := stripIndent(strings.Trim(strLit.Value, `"`))
-						f, err := os.CreateTemp("", "cuestr")
+					var (
+						v     string
+						names []string
+					)
+					switch l := field.Value.(type) {
+					case *ast.BasicLit:
+						v, _, err = stringValue(field.Value)
 						if err != nil {
-							errr = fmt.Errorf("failed to create temp file: %w", err)
 							return
 						}
-						if _, err := f.WriteString(v); err != nil {
-							errr = fmt.Errorf("failed to write to temp file: %w", err)
-							return
+					case *ast.Interpolation:
+						for _, e := range l.Elts {
+							str, n, err := stringValue(e)
+							names = append(names, n...)
+							if err != nil {
+								return
+							}
+							v += str
 						}
-						fmtcmd = strings.ReplaceAll(fmtcmd, c.placeholder, f.Name())
-						cmd := exec.Command(bin, "-c", fmtcmd)
-						cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", envFileKey, f.Name()))
-						if out, err := cmd.CombinedOutput(); err != nil {
-							errr = fmt.Errorf("failed to run %q: %s", fmtcmd, string(out))
-							return
-						}
-						b, err := os.ReadFile(f.Name())
-						if err != nil {
-							errr = fmt.Errorf("failed to read temp file: %w", err)
-							return
-						}
-						formatted := `"""` + "\n" + strings.Trim(string(b), "\n") + "\n" + `"""`
-						strLit.Value = formatted
+					default:
+						return
+					}
+
+					// multiline string literal only
+					if !strings.Contains(v, "\n") || !strings.Contains(v, `"""`) {
+						return
+					}
+
+					var (
+						repPairs    []string
+						revertPairs []string
+					)
+					for i, n := range names {
+						repPairs = append(revertPairs, fmt.Sprintf(`\(%s)`, n), fmt.Sprintf("cuestr%dRep", i))
+						revertPairs = append(repPairs, fmt.Sprintf("cuestr%dRep", i), fmt.Sprintf(`\(%s)`, n))
+					}
+					rep := strings.NewReplacer(repPairs...)
+					revert := strings.NewReplacer(revertPairs...)
+
+					v = rep.Replace(stripIndent(strings.Trim(v, `"`)))
+					f, err := os.CreateTemp("", "cuestr")
+					if err != nil {
+						errr = fmt.Errorf("failed to create temp file: %w", err)
+						return
+					}
+					if _, err := f.WriteString(v); err != nil {
+						errr = fmt.Errorf("failed to write to temp file: %w", err)
+						return
+					}
+					fmtcmd = strings.ReplaceAll(fmtcmd, c.placeholder, f.Name())
+					cmd := exec.Command(bin, "-c", fmtcmd)
+					cmd.Env = append(os.Environ(), fmt.Sprintf("%s=%s", envFileKey, f.Name()))
+					if out, err := cmd.CombinedOutput(); err != nil {
+						errr = fmt.Errorf("failed to run %q: %s", fmtcmd, string(out))
+						return
+					}
+					b, err := os.ReadFile(f.Name())
+					if err != nil {
+						errr = fmt.Errorf("failed to read temp file: %w", err)
+						return
+					}
+					formatted := revert.Replace(`"""` + "\n" + strings.Trim(string(b), "\n") + "\n" + `"""`)
+					field.Value = &ast.BasicLit{
+						ValuePos: field.Value.Pos(),
+						Kind:     token.STRING,
+						Value:    formatted,
 					}
 				}
 			}
@@ -121,4 +158,32 @@ func stripIndent(v string) string {
 	}
 
 	return strings.Join(lines, "\n")
+}
+
+func stringValue(v ast.Expr) (string, []string, error) {
+	var names []string
+	switch v := v.(type) {
+	case *ast.BasicLit:
+		if v.Kind == token.STRING {
+			return v.Value, nil, nil
+		}
+		return "", nil, fmt.Errorf("not a string literal: %v", v)
+	case *ast.SelectorExpr:
+		vx, names, err := stringValue(v.X)
+		if err != nil {
+			return "", nil, err
+		}
+		i, ok := v.Sel.(*ast.Ident)
+		if !ok {
+			return "", nil, fmt.Errorf("not an identifier: %v", v.Sel)
+		}
+		name := vx + "." + i.Name
+		names = append(names, name)
+		return name, names, nil
+	case *ast.Ident:
+		names = append(names, v.Name)
+		return v.Name, names, nil
+	default:
+		return "", nil, fmt.Errorf("not a string: %v", v)
+	}
 }
